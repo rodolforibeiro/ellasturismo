782708c93d7ce52b38d2b2f362434bc1ed546a6c
Rodolfo Ribeiro <rodolforibeiro1000@gmail.com>
2018-01-23 20:43:39 -0200

    nova versao

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -3,8 +3,8 @@ working_directory root
 
 pid "/var/www/ellascrm/shared/pids/unicorn.pid"
 
-stderr_path "/var/www/ellascrm//log/unicorn.log"
-stdout_path "/var/www/ellascrm//log/unicorn.log"
+stderr_path "/var/www/ellascrm/shared/log/unicorn.log"
+stdout_path "/var/www/ellascrm/shared/log/unicorn.log"
 
 worker_processes 4
 timeout 30
